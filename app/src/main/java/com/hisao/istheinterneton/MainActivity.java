9855745e91def54a1ffb1548b840f9df3f1b6fa5
instrutor <instrutor@targettrust.local>
2016-09-26 22:18:21 -0300

    Add easter egg

--- app/src/main/java/com/hisao/istheinterneton/MainActivity.java
+++ app/src/main/java/com/hisao/istheinterneton/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private static MainActivity mainActivityInstance;
+    private TextView txtAns;
+    private static int counter = 0;
     ConnectionSupport connectionSupport;
 
     @Override
@@ -19,11 +23,32 @@ public class MainActivity extends AppCompatActivity {
         mainActivityInstance = this;
         setContentView(R.layout.activity_main);
         setTitle(getResources().getString(R.string.app_name));
+        txtAns = (TextView) findViewById(R.id.txtAnswer);
+
+
+        txtAns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (counter > 5){
+                    counter = 0;
+                    ImageView image = new ImageView(MainActivity.this);
+                    image.setImageResource(R.drawable.photo);
+                    AlertDialog.Builder builder =
+                            new AlertDialog.Builder(MainActivity.this).
+                                    setView(image);
+                    builder.create().show();
+                }else{
+                    counter ++;
+                }
+            }
+        });
 
         final IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         connectionSupport = new ConnectionSupport();
         registerReceiver(connectionSupport, intentFilter);
+
+
     }
 
     public static MainActivity getInstance() {
@@ -60,7 +85,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Boolean aBoolean) {
                 super.onPostExecute(aBoolean);
-                TextView txtAns = (TextView) findViewById(R.id.txtAnswer);
                 if (aBoolean) {
                     txtAns.setText(getResources().getString(R.string.yes));
                 } else {
