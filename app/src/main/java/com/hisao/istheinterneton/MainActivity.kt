fbe8213644baca5dbb4583f2997e6af7767d3db8
Suzuki <vinicius.suzuki@hp.com>
2020-10-23 19:02:35 -0300

    From Java to Kotlin. Blank project

---
+++ app/src/main/java/com/hisao/istheinterneton/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hisao.istheinterneton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hisao.istheinterneton.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
